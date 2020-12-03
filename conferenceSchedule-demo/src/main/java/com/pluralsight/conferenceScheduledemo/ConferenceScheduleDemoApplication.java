52cf81a872d9fafd2b2fe59fc625eb12ff5ee115
Chris <christian.nymark@gmail.com>
2020-12-03 09:43:13 +0000

    initial spring setup with packages

---
+++ conferenceSchedule-demo/src/main/java/com/pluralsight/conferenceScheduledemo/ConferenceScheduleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferenceScheduledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceScheduleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceScheduleDemoApplication.class, args);
+	}
+
+}
---
+++ conferenceSchedule-demo/src/test/java/com/pluralsight/conferenceScheduledemo/ConferenceScheduleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferenceScheduledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceScheduleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
